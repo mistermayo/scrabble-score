d1fdedc4bdfcdfc842c667496dc928f83cdc53cd
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-12 14:12:43 -0800

    add logic and frontend

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,43 @@
-var scrabble = function(words) {
-  var letters = words.toLowerCase().split(" ");
+  var scrabble = function(word) {
+  var letters = word.toLowerCase().split("");
   var points = 0;
   var score = 0;
+
   var scrabblePoints = {
 
-    a: 1, e: 1, i: 1, o: 1, u: 1, l: 1,
-    n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3,
-    p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10
-
+    'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1,
+    'n': 1, 'r': 1, 's': 1, 't': 1, 'd': 2, 'g': 2, 'b': 3, 'c': 3,
+    'm': 3, 'p': 3, 'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4, 'k': 5,
+    'j': 8, 'x': 8, 'q': 10, 'z': 10
     };
-    
+
     letters.forEach(function(letter) {
-      points += scrabblePoints
+      score = scrabblePoints[letter];
+      points +=score;
+
     });
 
+    return points;
 };
+
+$(function() {
+
+  $("form#scrabble").submit(function(event) {
+    $(".result").text("");
+    $("#result").hide();
+    $("#error").hide();
+
+    var word = $("input#word").val();
+
+    var score = scrabble(word)
+
+    $(".result").text(score);
+
+      if (isNaN(score)) {
+        $("#error").show();
+    } else if (word) {
+        $("#result").show();
+    }
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,9 @@
-describe('scrabble', function() {
-  it("return score for a single letter", function(){
+describe("scrabble", function() {
+  it("returns score for a single letter", function(){
     expect(scrabble("d")).to.equal(2);
   });
 
-  it("return score for a full word", function() {
+  it("returns score for a full word", function() {
     expect(scrabble("Cat")).to.equal(5);
   });
-
 });
